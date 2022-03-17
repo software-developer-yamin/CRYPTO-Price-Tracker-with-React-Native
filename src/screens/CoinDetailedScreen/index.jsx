a3f11d835af8ce388b3ea2de205f4a82a329fe9f
DeveloperYamin <developer.yamin@gmail.com>
2022-03-17 22:14:15 +0600

    provide props in CoinDetailHeader component;

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -1,10 +1,22 @@
 import { View } from "react-native";
+import Coin from "../../../assets/data/crypto.json";
 import CoinDetailHeader from "./components/CoinDetailHeader";
 
 const index = () => {
+  const {
+    image: { small },
+    name,
+    symbol,
+    market_data: { market_cap_rank },
+  } = Coin;
+
   return (
     <View>
-      <CoinDetailHeader />
+      <CoinDetailHeader
+        image={small}
+        symbol={symbol}
+        marketCapRank={market_cap_rank}
+      />
     </View>
   );
 };

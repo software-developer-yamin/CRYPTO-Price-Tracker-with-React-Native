34389b500af6e89fa44fbb8c0a9a56b3a1d44787
DeveloperYamin <developer.yamin@gmail.com>
2022-03-17 19:24:41 +0600

    creating CoinDetailedScreen

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import HomeScreen from "./src/screens/HomeScreen";
+import CoinDetailedScreen from "./src/screens/CoinDetailedScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen />
+      <CoinDetailedScreen />
       <StatusBar style="light" />
     </View>
   );
---
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const index = () => {
+  return (
+    <View>
+      <Text style={{ color: "white" }}>index</Text>
+    </View>
+  );
+};
+
+export default index;
